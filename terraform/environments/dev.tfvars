64a2111249d41c838b4ea3918f2eb2c440a79e4c
Aditya Dixit <dixit99304@gmail.com>
2026-09-06 01:39:57 +0530

    Added terraform files for this app.

---
+++ terraform/environments/dev.tfvars
@@ -0,0 +1,4 @@
+aws_region       = "ap-south-1"
+project_name     = "writing-assistant"
+environment      = "dev"
+bedrock_model_id = "zai.glm-5"
---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "writing-assistant"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "bedrock_model_id" {
+  description = "Amazon Bedrock model ID"
+  type        = string
+  default     = "zai.glm-5"
+}
